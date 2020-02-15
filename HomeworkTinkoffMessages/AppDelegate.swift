6c98d2b13ec9502bfb79a74b83d8a35197de171e
goaIKPI <44200695+goaIKPI@users.noreply.github.com>
2020-02-15 20:29:20 +0700

    initial commit

---
+++ HomeworkTinkoffMessages/AppDelegate.swift
@@ -0,0 +1,47 @@
+//
+//  AppDelegate.swift
+//  HomeworkTinkoffMessages
+//
+//  Created by Олег Герман on 13.02.2020.
+//  Copyright © 2020 Олег Герман. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        print("Application moved from Not running to Inactive: application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?)")
+        return true
+    }
+    
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        print("Application moved from Active to Inactive: applicationWillResignActive")
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        print("Application moved from Inactive to Active: applicationDidBecomeActive")
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        print("Application moved from Inactive to Background: applicationDidEnterBackground")
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        print("Application moved from Background to Inactive: applicationWillEnterForeground")
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        print("Application moved from Background to Not Running: applicationWillTerminate")
+    }
+    
+    
+    
+    
+}
+
---
+++ HomeworkTinkoffMessages/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  HomeworkTinkoffMessages
+//
+//  Created by Олег Герман on 13.02.2020.
+//  Copyright © 2020 Олег Герман. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("viewDidLoad")
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("viewWillAppear")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("viewDidAppear")
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print("viewWillLayoutSubviews")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print("viewDidLayoutSubviews")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewDidDisappear")
+    }
+    
+}
+
