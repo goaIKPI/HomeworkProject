9ea193e06e6bcb9df17e481df33b50dfa61fb713
goaIKPI <44200695+goaIKPI@users.noreply.github.com>
2020-02-16 22:16:30 +0300

    fixed warning with AppIcon

--- HomeworkTinkoffMessages/ViewController.swift
+++ HomeworkTinkoffMessages/ViewController.swift
@@ -12,38 +12,37 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewDidLoad")
         // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        print("viewWillAppear")
+        print("Application moved from Disappeared | Disappearing to Appearing: viewWillAppear")
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        print("viewDidAppear")
+        print("Application moved from Appearing to Appeared: viewDidAppear")
     }
     
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        print("viewWillLayoutSubviews")
+        print("Application moved from Autolayouted to Autolayouting: viewWillLayoutSubviews")
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        print("viewDidLayoutSubviews")
+        print("Application moved from Autolayouting to Autolayouted: viewDidLayoutSubviews")
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        print("viewWillDisappear")
+        print("Application moved from Appearing | Appeared to Disappearing: viewWillDisappear")
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        print("viewDidDisappear")
+        print("Application moved from Disappearing to Disappeared: viewWillAppear")
     }
     
 }
